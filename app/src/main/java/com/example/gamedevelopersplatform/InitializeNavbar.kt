d535f010cfe7963940c7c62b64f2ef9fc19b6724
Lior Hassin <liorhassin3@gmail.com>
2024-03-25 23:12:48 +0200

    user signup working with firebase and firestore

--- app/src/main/java/com/example/gamedevelopersplatform/InitializeNavbar.kt
+++ app/src/main/java/com/example/gamedevelopersplatform/InitializeNavbar.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class InitializeNavbar : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_initialize_navbar)
+
         //Loop:
         //Initialize the navbar functionality.
         //move to homepage.
--- app/src/main/java/com/example/gamedevelopersplatform/SignUpPage.kt
+++ app/src/main/java/com/example/gamedevelopersplatform/SignUpPage.kt
@@ -1,6 +1,9 @@
 package com.example.gamedevelopersplatform
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,7 +22,55 @@ class SignUpPage : AppCompatActivity() {
         binding = ActivitySignUpPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
+        binding.signUpButton.setOnClickListener {
+            val nickname = binding.nicknameInput.text.toString()
+            val password = binding.passwordInput.text.toString()
+            val email = binding.emailInput.text.toString()
+            if(validation(nickname, password, email)){
+                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        saveUserToFirestore(nickname, password, email)
+                        Toast.makeText(this, "Account created successfully", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, InitializeNavbar::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                    else Toast.makeText(this, it.exception?.message.toString(),Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                //TODO - Show red error where validation failed.
+            }
+        }
 
     }
+
+    fun validation(nickname:String, password:String, email:String): Boolean{
+        return true
+    }
+
+    fun saveUserToFirestore(nickname:String, password:String, email:String){
+        val user = hashMapOf(
+            "Nickname" to nickname,
+            "Password" to password,
+            "Email" to email
+        )
+        val currentUser = firebaseAuth.currentUser
+        if(currentUser!=null){
+            val userId = currentUser.uid
+            firestore.collection("Users").document(userId).set(user).addOnCompleteListener {
+                if(it.isSuccessful){
+                    Log.d("success", "User was saved successfully to Firestore")
+                }
+                else{
+                    Log.d("fail", it.exception?.message.toString())
+                }
+            }
+        }
+    }
+
+    //TODO - Add room (local cache)
 }

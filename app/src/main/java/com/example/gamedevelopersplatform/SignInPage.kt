96235034a25c302b6c76fa6c7ba4d5e282e25e1e
Lior Hassin <liorhassin3@gmail.com>
2024-03-25 21:01:26 +0200

    navbar layout + home fragment base

--- app/src/main/java/com/example/gamedevelopersplatform/SignInPage.kt
+++ app/src/main/java/com/example/gamedevelopersplatform/SignInPage.kt
@@ -1,11 +1,14 @@
 package com.example.gamedevelopersplatform
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SignInPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_in_page)
     }
 }

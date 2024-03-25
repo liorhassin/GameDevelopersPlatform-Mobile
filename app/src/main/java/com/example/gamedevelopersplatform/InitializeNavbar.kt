9a1d7cfaa9f03a8a71716c2dc1e75000f86c9c8e
Lior Hassin <liorhassin3@gmail.com>
2024-03-25 23:24:05 +0200

    Initialize navbar functionallity (moving between fragments)

--- app/src/main/java/com/example/gamedevelopersplatform/InitializeNavbar.kt
+++ app/src/main/java/com/example/gamedevelopersplatform/InitializeNavbar.kt
@@ -1,17 +1,60 @@
 package com.example.gamedevelopersplatform
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.gamedevelopersplatform.databinding.ActivityInitializeNavbarBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class InitializeNavbar : AppCompatActivity() {
+    private lateinit var binding: ActivityInitializeNavbarBinding
 
+    private  lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_initialize_navbar)
 
-        //Loop:
-        //Initialize the navbar functionality.
-        //move to homepage.
+        binding = ActivityInitializeNavbarBinding.inflate(layoutInflater)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        setContentView(binding.root)
+        replaceFragment(HomePage())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomePage())
+                R.id.profile -> replaceFragment(ProfilePage())
+                R.id.addProject -> replaceFragment(AddProjectPage())
+                R.id.myProjects -> replaceFragment(MyProjectsPage())
+                R.id.logout -> {
+                    if(firebaseAuth.currentUser != null){
+                        val builder = AlertDialog.Builder(this)
+                        builder.setPositiveButton("Yes"){_,_->
+                            firebaseAuth.signOut()
+                            Toast.makeText(this, "Logout...", Toast.LENGTH_SHORT).show()
+                            val intent = Intent(this, SignInPage::class.java)
+                            startActivity(intent)
+                            finish()
+                        }
+                        builder.setNegativeButton("No"){_,_-> }
+                        builder.setTitle("Logout?")
+                        builder.create().show()
+                    }
+                }
+                else ->{
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }

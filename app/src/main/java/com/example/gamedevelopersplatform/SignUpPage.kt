ed4cb325112963067f99ee55a9dc8040e9d787cb
Lior Hassin <liorhassin3@gmail.com>
2024-03-25 22:21:40 +0200

    firebase connection to signup page + binding

--- app/src/main/java/com/example/gamedevelopersplatform/SignUpPage.kt
+++ app/src/main/java/com/example/gamedevelopersplatform/SignUpPage.kt
@@ -5,16 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.gamedevelopersplatform.databinding.ActivitySignUpPageBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class SignUpPage : AppCompatActivity() {
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var binding: ActivitySignUpPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivitySignUpPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }

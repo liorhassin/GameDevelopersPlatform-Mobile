0a362f7a0713fb883cde80782af209d9a87a0a84
Lior Hassin <liorhassin3@gmail.com>
2024-03-25 21:48:42 +0200

    initialized fragments + project arrangement

---
+++ app/src/main/java/com/example/gamedevelopersplatform/InitializeNavbar.kt
@@ -0,0 +1,11 @@
+package com.example.gamedevelopersplatform
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class InitializeNavbar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initialize_navbar)
+    }
+}

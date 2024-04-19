3a6e8a81712f05d1958886180fb0e4aa9e2d8a6c
Lior Hassin <86710692+liorhassin@users.noreply.github.com>
2024-04-19 14:57:09 +0300

    todo

--- app/src/main/java/com/example/gamedevelopersplatform/data/GameData.kt
+++ app/src/main/java/com/example/gamedevelopersplatform/data/GameData.kt
@@ -1,5 +1,8 @@
 package com.example.gamedevelopersplatform.data
 
+import androidx.room.Entity
+//TODO - Check if changing to entity still works with the entire project structure and usage.
+@Entity(tableName = "game_table")
 data class GameData(
     var image:String = ""
     , var name:String = ""

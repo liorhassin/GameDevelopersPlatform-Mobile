959d76d21b757251d85a0aade159fa088045b63a
Lior Hassin <liorhassin3@gmail.com>
2024-06-11 00:08:15 +0300

    addons

--- app/src/main/java/com/example/gamedevelopersplatform/entity/Game.kt
+++ app/src/main/java/com/example/gamedevelopersplatform/entity/Game.kt
@@ -1,6 +1,6 @@
 package com.example.gamedevelopersplatform.entity
 
-import androidx.room.ColumnInfo
+import giandroidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
